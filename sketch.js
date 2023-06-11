703dd5f6c2ce774eef3e15b6cbad0b89b9edcc13
Daniel White <dm.white@gmx.com>
2023-06-11 23:04:29 +0100

    Randomly rotating square

--- sketch.js
+++ sketch.js
@@ -10,8 +10,12 @@ function draw() {
     background(0);
     var nX = noise(time);
     var locX = map(nX, 0, 1, 0, width);
+    var g = map(nX, 0 , 1, 0, 255);
+
+    var rotZ = map(nX, 0, 1, -25, 25);
     translate(locX, height/2);
-    fill(255);
+    rotate(rotZ);
+    fill(0, g, 0);
     rect(0, 0, 100, 100);
     time+=0.01;
 }
