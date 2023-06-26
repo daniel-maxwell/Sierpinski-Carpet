66001db4fb0e2ac34d5f6dcee584a9f1ce6d6c8e
Daniel White <dm.white@gmx.com>
2023-06-26 11:42:53 +0100

    Fracral Implementation

--- sketch.js
+++ sketch.js
@@ -1,24 +1,67 @@
-var time = 1000;
-
+var startSize;
+var functionCalls=0;
 function setup() {
-    createCanvas(900, 600);
+    createCanvas(900, 900);
     background(0);
+    fill(255);
+    noStroke();
+    noSmooth();
     rectMode(CENTER);
+    startSize = pow(3, 6); // 3 ^ 6
 }
 
 function draw() {
-    background(0);
-    var nX = noise(time);
-    var locX = map(nX, 0, 1, 0, width);
-    var g = map(nX, 0 , 1, 0, 255);
+    translate(width/2, height/2);
+    mySquare(startSize);
+    noLoop();
+}
 
-    var nY = noise(time + 5);
-    var locY = map(nY, 0, 1, 0, height);
+function mySquare(side) {
+    functionCalls++;
+    side = side/3;
 
-    var rotZ = map(nX, 0, 1, -25, 25);
-    translate(locX, locY);
-    rotate(rotZ);
-    fill(0, g, 0);
-    rect(0, 0, 100, 100);
-    time+=0.01;
+    if (side>=1){
+        rect(0, 0, side, side);
+
+        push();
+        translate(-side, 0);
+        mySquare(side);
+        pop();
+        
+        push();
+        translate(-side, -side);
+        mySquare(side);
+        pop();
+
+        push();
+        translate(0, -side);
+        mySquare(side);
+        pop();
+
+        push();
+        translate(side, -side);
+        mySquare(side);
+        pop();
+
+        push();
+        translate(side, 0);
+        mySquare(side);
+        pop();
+
+        push();
+        translate(side, side);
+        mySquare(side);
+        pop();
+
+        push();
+        translate(0, side);
+        mySquare(side);
+        pop();
+
+        push();
+        translate(-side, side);
+        mySquare(side);
+        pop();
+    }
+
 }
